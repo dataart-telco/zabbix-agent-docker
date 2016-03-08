d8f9118e6dbe02d17055151b52e61ee824e156b4
Gennadiy Dubina <gdubina@dataart.com>
2016-03-08 18:25:21 +0200

    support ZA_ template

--- files/start_agent.sh
+++ files/start_agent.sh
@@ -2,9 +2,16 @@
 
 CONFIG_FILE='/etc/zabbix/zabbix_agentd.conf'
 
-if [[ -n "$ZABBIX_SERVER" ]]; then
-  echo "Update zabix server to $ZABBIX_SERVER"
-  sed -i 's/Server=127.0.0.1/Server='$ZABBIX_SERVER'/g' ${CONFIG_FILE}
-fi
+for i in $( set -o posix ; set | grep ^ZA_ | sort -rn ); do
+    reg=$(echo ${i} | cut -d = -f1 | cut -c 4-)
+    val=$(echo ${i} | cut -d = -f2)
+    FIND_RESULT=`grep ^${reg}= $CONFIG_FILE`
+    if [[ -n "$FIND_RESULT" ]]; then
+        sed -i "s|^${reg}=.*|${reg}=${val}|g" $CONFIG_FILE 
+    else
+        echo "# Auto generated: $i"
+        echo "${reg}=${val}" >> $CONFIG_FILE
+    fi
+done
 
 zabbix_agentd -c ${CONFIG_FILE}
